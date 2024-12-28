3717695805ee90d8d66d685cce8be4786d4ee4f8
CJ-404 <cj.ptsz@yahoo.com>
2024-12-28 22:15:05 +0530

    recap: singleton pattern eager version

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import singletone.SingletonLazy;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        // singleton patten eager
+        /*
+        SingletonEager obj = SingletonEager.getInstance();
+        System.out.println(obj.hashCode());
+        SingletonEager obj2 = SingletonEager.getInstance();
+        System.out.println(obj2.hashCode());
+        */
+    }
+}
---
+++ src/main/java/singletone/SingletonEager.java
@@ -0,0 +1,12 @@
+package singletone;
+
+public class SingletonEager {
+
+    private static final SingletonEager instance = new SingletonEager();
+    private SingletonEager(){}
+
+    public static SingletonEager getInstance()
+    {
+        return instance;
+    }
+}
